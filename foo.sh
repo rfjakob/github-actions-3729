b68d7b5f9b143c7923d101f00d0ef86b81d22c69
Jakob Unterwurzacher <jakobunt@gmail.com>
2021-07-15 16:03:27 +0200

    10gb

--- foo.sh
+++ foo.sh
@@ -3,7 +3,7 @@ set -ux
 
 ls -l /proc/self/fd
 
-dd if=/dev/zero of=- bs=1M count=1 >&106 &
-dd if=/dev/zero of=- bs=1M count=1 >&109 &
+dd if=/dev/zero of=- bs=1M count=10000 >&106 &
+dd if=/dev/zero of=- bs=1M count=10000 >&109 &
 
 sleep 3
