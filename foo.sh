0171f33a502efb84c3d493c3f081b940ac2e3dfa
Jakob Unterwurzacher <jakobunt@gmail.com>
2021-07-15 16:05:45 +0200

    pv

--- foo.sh
+++ foo.sh
@@ -3,7 +3,7 @@ set -ux
 
 ls -l /proc/self/fd
 
-dd if=/dev/zero of=- bs=1M count=10000 >&106 &
-dd if=/dev/zero of=- bs=1M count=10000 >&109 &
+timeout 20s pv < /dev/urandom >&106 &
+timeout 20s pv < /dev/urandom >&109 &
 
-sleep 3
+wait
