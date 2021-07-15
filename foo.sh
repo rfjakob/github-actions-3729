01ea369760dc72ecbdcee162a87181a92dd69f21
Jakob Unterwurzacher <jakobunt@gmail.com>
2021-07-15 16:07:28 +0200

    cat

--- foo.sh
+++ foo.sh
@@ -3,7 +3,11 @@ set -ux
 
 ls -l /proc/self/fd
 
-timeout 20s pv < /dev/urandom >&106 &
-timeout 20s pv < /dev/urandom >&109 &
+timeout 20s cat /dev/urandom >&106 &
+timeout 20s cat /dev/urandom >&109 &
+
+ps auxwww
+
+ps auxwww --forest
 
 wait
